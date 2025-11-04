63989ae6e905c94d659d1e5c1b49fa8c1d992687
Wim Jeantine-Glenn <hey@wimglenn.com>
2025-11-03 20:54:14 -0600

    1

---
+++ ec_wim/ec2025/q01.py
@@ -0,0 +1,23 @@
+from ecd import data
+
+for part, chunks in data.items():
+    names, steps = chunks.split("\n\n")
+    names = names.split(",")
+    steps = [int(x) for x in steps.replace("R", "").replace("L", "-").split(",")]
+    N = len(names)
+    if part == "1":
+        i = 0
+        for step in steps:
+            i += step
+            if i < 0:
+                i = 0
+            if i >= N:
+                i = N - 1
+        print(f"Part {part}:", names[i])
+    if part == "2":
+        print(f"Part {part}:", names[sum(steps) % N])
+    if part == "3":
+        for step in steps:
+            i = step % N
+            names[0], names[i] = names[i], names[0]
+        print(f"Part {part}:", names[0])
